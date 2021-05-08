d8d05703256cab22f044298043bdc6985bacf056
doppelganger5 <83863514+doppelganger5@users.noreply.github.com>
2021-05-08 15:37:06 +0530

    Update script.js

--- infonow/js/script.js
+++ infonow/js/script.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
     var coinURL = 'https://api.nomics.com/v1/currencies/ticker?key=28a45504e7a0e0963495e7301bf1a885&sort=rank&interval=1d&convert=USD&per-page=10&page=1'
     $.ajax({
         url: coinURL,
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
+        data: myData,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function() { alert("Success"); },
+        error: function() { alert('Failed!'); },
+        beforeSend: setHeader
     type: "POST"
     }).done(function (response) {
         console.log(response)
