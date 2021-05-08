59536b4eb31849607055a72984d12868c94919cc
doppelganger5 <83863514+doppelganger5@users.noreply.github.com>
2021-05-08 15:42:06 +0530

    Update script.js

--- infonow/js/script.js
+++ infonow/js/script.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
     var coinURL = 'https://api.nomics.com/v1/currencies/ticker?key=28a45504e7a0e0963495e7301bf1a885&sort=rank&interval=1d&convert=USD&per-page=10&page=1'
     $.ajax({
         url: coinURL,
-        data: myData,
+        //data: myData,
         type: 'GET',
         crossDomain: true,
-        dataType: 'jsonp',
+        //dataType: 'jsonp',
         success: function() { alert("Success"); },
         error: function() { alert('Failed!'); },
-        beforeSend: setHeader
+        //beforeSend: setHeader
     type: "POST"
     }).done(function (response) {
         console.log(response)
